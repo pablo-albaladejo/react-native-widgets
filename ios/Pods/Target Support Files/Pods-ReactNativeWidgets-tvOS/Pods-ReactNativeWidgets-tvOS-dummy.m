b01d13116d2f1ecfb58d78ba65023694446828c8
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-09-04 12:56:04 +0200

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeWidgets-tvOS/Pods-ReactNativeWidgets-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeWidgets_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeWidgets_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeWidgets/Pods-ReactNativeWidgets-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeWidgets : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeWidgets
+@end
