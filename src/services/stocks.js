0daac5caf6ca6a9ee2f7f71adc84f0d82794e640
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-09-06 14:32:49 +0200

    feat(master); quote API request added

--- src/services/stocks.js
+++ src/services/stocks.js
@@ -14,10 +14,49 @@ export const searchStock = (str) => {
             }
         })
             .then(res => {
-                resolve(res.bestMatches);
+                if(res['Note']) reject(res['Note'])
+                resolve(transformArrayResponse(res['bestMatches']));
 
             }).catch(e => {
                 reject(e)
             })
     })
-} 
+}
+
+export const getStockQuote = (symbol) => {
+    return new Promise((resolve, reject) => {
+        apiFetch({
+            baseUrl: config.api.baseUrl,
+            endPoint: config.api.endpoints.query,
+            method: FETCH_METHODS.GET,
+            params: {
+                function: config.api.params.function.GLOBAL_QUOTE,
+                symbol,
+                apikey: config.api.key,
+            }
+        })
+            .then(res => {
+                if(res['Note']) reject(res['Note'])
+                resolve(transformResponse(res["Global Quote"]));
+            }).catch(e => {
+                reject(e)
+            })
+    })
+}
+
+const transformArrayResponse = (arrayResponse) => {
+    let output = [];
+    arrayResponse.forEach(response => {
+        output.push(transformResponse(response))
+    })
+    return output;
+}
+
+const transformResponse = (response) => {
+    let output = {};
+    Object.keys(response).forEach(key => {
+        output[key.split(' ')[1]] = response[key]
+    })
+    output.updated = (new Date()).toLocaleTimeString();
+    return output;
+}
