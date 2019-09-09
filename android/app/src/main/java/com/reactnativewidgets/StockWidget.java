d0395ec848b5a48e5f8e14ae32e3cea6709e9c63
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-09-09 13:18:37 +0200

    feat(master): basic widget layout

--- android/app/src/main/java/com/reactnativewidgets/StockWidget.java
+++ android/app/src/main/java/com/reactnativewidgets/StockWidget.java
@@ -13,10 +13,15 @@ public class StockWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence symbolText = "APPL";
+        CharSequence openText = "214.05";
+        CharSequence priceText = "213.26";
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_symbol, symbolText);
+        views.setTextViewText(R.id.appwidget_open, openText);
+        views.setTextViewText(R.id.appwidget_price, priceText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
